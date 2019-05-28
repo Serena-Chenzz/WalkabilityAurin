55120bec9f0dbb8136e39244a4a9e59e6ad89bf9
Serena-Chenzz <clcltt@163.com>
2019-05-28 02:25:15 +0000

    clean files

--- Walkability-5points1/Walkability-5points/README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-# Walkability
-=======
-# Walkability-beta
->>>>>>> 5abcffa6f6483caf77b6ee67757917aeda5e5ddc
--- Walkability-5points1/Walkability-5points/docker/start_density.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-./apache-activemq-5.15.6/bin/activemq start
-
-mvn exec:java -Dexec.mainClass="org.mccaughey.GenerateDensity"
